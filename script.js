a3c9c4750a63d9a78651d76d79fd5797f4b1ef8b
Amol shelke <shelkeamol897@gmail.com>
2022-08-17 22:24:33 +0530

    Updated Script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// JavaScript
+const ratings = document.querySelectorAll(".rating");
+const ratingContainer = document.querySelector(".ratings-container");
+const sendBtn = document.querySelector("#send");
+const panel = document.querySelector("#panel");
+
+let selectRating = "Satisfied";
+
+sendBtn.addEventListener("click", (e) => {
+  panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank You</strong>
+        <br>
+        <strong>Feedback: ${selectRating} </strong>
+        <p>We'll use your feedback to improve our customer support</p>
+    `;
+});
+
+function removeActive() {
+  for (let i = 0; i < ratings.length; i++) {
+    ratings[i].classList.remove("active");
+  }
+}
